c47f7e78d25705afb8cd34633f863842a6a24204
Ultramari <zykovamari2016@yandex.ru>
2023-03-30 11:11:05 +0300

    исправила ошибки после 1 ревью

--- scripts/index.js
+++ scripts/index.js
@@ -1,21 +1,20 @@
 //попап профиля
 //константы попапа изменения профиля
-const popupElement = document.querySelector('.popup'); //doom попапа
 const popupProfileElement = document.querySelector('.popup_profile-info'); //попап информации профиля
-const popupCloseButtonElement = popupProfileElement.querySelector('.popup__close-button'); //кнопка закрытия попапа
-const popupOpenButtonElement = document.querySelector('.profile__info-button'); //кнопка открытия попапа профиля
-const formElement = document.querySelector('.popup__form') // область формы для сабмита
-const nameInput = popupElement.querySelector('.popup__text-input_type_name'); //инпут имени в попапе профиля
-const jobInput = popupElement.querySelector('.popup__text-input_type_description'); //инпут описания в попапе профиля
+const closeProfilePopupButtonElement = popupProfileElement.querySelector('.popup__close-button'); //кнопка закрытия попапа
+const openProfilePopupButtonElement = document.querySelector('.profile__info-button'); //кнопка открытия попапа профиля
+const formForSubmitProfileElement = popupProfileElement.querySelector('.popup__form') // область формы для сабмита
+const nameInput = popupProfileElement.querySelector('.popup__text-input_type_name'); //инпут имени в попапе профиля
+const jobInput = popupProfileElement.querySelector('.popup__text-input_type_description'); //инпут описания в попапе профиля
 const profileNameElement = document.querySelector('.profile__name'); // имя профиля на странице
 const profileDescriptionElement = document.querySelector('.profile__subtitle'); //описание профиля на странице
 
 //отправка формы
-function handleFormSubmit(evt) {
+function handleProfileFormSubmit(evt) {
   evt.preventDefault(); // запрет отправки на серв
   profileNameElement.textContent = nameInput.value; //замена имени на имя из инпута
   profileDescriptionElement.textContent = jobInput.value; //замена описания на описание из инпута
-  closePopup(); // закрыть попап после сабмита
+  closeProfilePopup(); // закрыть попап после сабмита
 };
 
 //универсальная функция открытия попапа
@@ -43,15 +42,14 @@ function closeProfilePopup() {
 //попап карточек мест
 //константы добавления карточек мест
 const popupPlaceCardElement = document.querySelector('.popup_add-place'); //попап добавления карточки места
-const popupCloseButtonPlaceElement = popupPlaceCardElement.querySelector('.popup__close-button'); //кнопка закрытия попапа карточки места
-const placeButtonElement = document.querySelector('.profile__button'); //кнопка с плюсом для добавления карточки места
+const closePlacePopupButtonElement = popupPlaceCardElement.querySelector('.popup__close-button'); //кнопка закрытия попапа карточки места
+const openPlacePopupButtonElement = document.querySelector('.profile__button'); //кнопка с плюсом для добавления карточки места
 const placeInput = popupPlaceCardElement.querySelector('.popup__text-input_type_place'); //ипут названия места
 const imgInput = popupPlaceCardElement.querySelector('.popup__text-input_type_link'); //инпут ссылки на картинку
-const submitPlaceButton = popupPlaceCardElement.querySelector('.popup__submit-button'); //кнопка отправки формы в попапе добавления карточки
 const templateElement = document.querySelector('#element-template').content;
 const placesList = document.querySelector('.elements-grid'); //куда добавляются все карточки
-const formSubmitPlaceElement = popupPlaceCardElement.querySelector('.popup__form_add-card'); //форма для сабмита
-const likeButton = document.querySelector('.element__info-like'); //кнопка лайка
+const formForSubmitPlaceElement = popupPlaceCardElement.querySelector('.popup__form_add-card'); //форма для сабмита
+const likeButton = popupPlaceCardElement.querySelector('.element__info-like'); //кнопка лайка
 
 //открыть попап с добавлением карточки
 function openPlacePopup() {
@@ -65,34 +63,6 @@ function closePlacePopup() {
 function likeCard(evt) {
   evt.target.classList.toggle('element__info-like_active');
 }
-
-//массив с картинками, которые будут изначально
-const initialCards = [
-  {
-    name: 'Архыз',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
-  },
-  {
-    name: 'Челябинская область',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
-  },
-  {
-    name: 'Иваново',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
-  },
-  {
-    name: 'Камчатка',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
-  },
-  {
-    name: 'Холмогорский район',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
-  },
-  {
-    name: 'Байкал',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
-  }
-];
 
 //константы попапа с открытием картинки
 const openedImageElement = document.querySelector('.image-popup');
@@ -135,7 +105,7 @@ initialCards.forEach((item) => {
   placesList.append(placeCard);
 })
 
-formSubmitPlaceElement.addEventListener('submit', (evt) => {
+formForSubmitPlaceElement.addEventListener('submit', (evt) => {
   evt.preventDefault(); // запрет отправки на серв
   const objectNamedUrl = {name: placeInput.value, link: imgInput.value};
   placesList.prepend(createCard(objectNamedUrl));
@@ -143,9 +113,9 @@ formSubmitPlaceElement.addEventListener('submit', (evt) => {
   evt.target.reset();
 });
 
-popupOpenButtonElement.addEventListener('click', openProfilePopup); //по клику на кнопку карандаша попап профиля открывается
-popupCloseButtonElement.addEventListener('click', closeProfilePopup); //по клику на крест попап профиля закрывается
-placeButtonElement.addEventListener('click', openPlacePopup); //по клику на кнопку плюса попап карточки открывается
-popupCloseButtonPlaceElement.addEventListener('click', closePlacePopup); //по клику на кнопку крестика попап карточки закрывается
+openProfilePopupButtonElement.addEventListener('click', openProfilePopup); //по клику на кнопку карандаша попап профиля открывается
+closeProfilePopupButtonElement.addEventListener('click', closeProfilePopup); //по клику на крест попап профиля закрывается
+openPlacePopupButtonElement.addEventListener('click', openPlacePopup); //по клику на кнопку плюса попап карточки открывается
+closePlacePopupButtonElement.addEventListener('click', closePlacePopup); //по клику на кнопку крестика попап карточки закрывается
 closeImageElementButton.addEventListener('click', closeImagePopup);
-formElement.addEventListener('submit', handleFormSubmit); //по клику отправляется форма профиля
+formForSubmitProfileElement.addEventListener('submit', handleProfileFormSubmit); //по клику отправляется форма профиля
